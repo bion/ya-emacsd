8a806d5de63e42c8f57cc710d541d5b08e9ec91b
bion <bionjohnson@gmail.com>
2018-06-07 07:35:57 -0700

    Delete stale packages

---
+++ packages/simp-20180606.1954/simp-autoloads.el
@@ -0,0 +1,74 @@
+;;; simp-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "simp-project" "simp-project.el" (0 0 0 0))
+;;; Generated autoloads from simp-project.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "simp-project" '("simp-")))
+
+;;;***
+
+;;;### (autoloads nil "simp-project-files" "simp-project-files.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from simp-project-files.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "simp-project-files" '("simp-project-")))
+
+;;;***
+
+;;;### (autoloads nil "simp-project-git" "simp-project-git.el" (0
+;;;;;;  0 0 0))
+;;; Generated autoloads from simp-project-git.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "simp-project-git" '("simp-project-")))
+
+;;;***
+
+;;;### (autoloads nil "simp-project-ibuffer" "simp-project-ibuffer.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from simp-project-ibuffer.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "simp-project-ibuffer" '("simp-project-")))
+
+;;;***
+
+;;;### (autoloads nil "simp-project-rgrep" "simp-project-rgrep.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from simp-project-rgrep.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "simp-project-rgrep" '("simp-project-")))
+
+;;;***
+
+;;;### (autoloads nil "simp-project-root-dired" "simp-project-root-dired.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from simp-project-root-dired.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "simp-project-root-dired" '("simp-project-root-dired")))
+
+;;;***
+
+;;;### (autoloads nil "simp-project-with-bookmarks" "simp-project-with-bookmarks.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from simp-project-with-bookmarks.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "simp-project-with-bookmarks" '("simp-project-")))
+
+;;;***
+
+;;;### (autoloads nil nil ("simp-pkg.el" "simp.el") (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; simp-autoloads.el ends here
---
+++ packages/simp-20180606.1954/simp-pkg.el
@@ -0,0 +1,6 @@
+(define-package "simp" "20180606.1954" "Simple project definition, chiefly for file finding, and grepping" 'nil :keywords
+  '("project" "grep" "find")
+  :url "https://github.com/re5et/simp")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
