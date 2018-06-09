724d9fc17bc01e2f638633af8450d984f2a4d356
bion <bionjohnson@gmail.com>
2018-06-08 18:02:15 -0700

    Add smart-indent-rigidly

---
+++ packages/smart-indent-rigidly-20141205.1615/smart-indent-rigidly-autoloads.el
@@ -0,0 +1,30 @@
+;;; smart-indent-rigidly-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "smart-indent-rigidly" "smart-indent-rigidly.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from smart-indent-rigidly.el
+
+(autoload 'smart-indent-rigidly-mode "smart-indent-rigidly" "\
+Un/Indent region if active or current line
+
+\\{smart-indent-rigidly-mode-map}
+
+\(fn &optional ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "smart-indent-rigidly" '("smart-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; smart-indent-rigidly-autoloads.el ends here
---
+++ packages/smart-indent-rigidly-20141205.1615/smart-indent-rigidly-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "smart-indent-rigidly" "20141205.1615" "Smart rigid indenting" 'nil :stars '(#("2" 0 1 (font-lock-face paradox-star-face))) :commit "323d1fe4d0b81e598249aad01bc44adb180ece0e" :keywords '("indenting" "coffee-mode" "haml-mode" "sass-mode") :url "https://github.com/re5et/smart-indent-rigidly")
